269238209a3f0f4034d26ccb4eba61e71a7fb427
NathalieW <nathalie.wang@isep.fr>
2017-01-16 15:52:34 +0100

    Change Note type to .txt & upgrate deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,3 +2,8 @@
 #Replace xxx by IP of your droplet
 tail -n +3 "$0" | ssh root@xxxx ; exit
 set -eu
+
+#update the server (~# apt-get update)
+#upgrade (~# apt-get upgrade)
+#install nginx (apt-get install nginx)
+#send the list of processes to /var/www/index.html (ps>/var/www/html/index.html)
