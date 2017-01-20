945adbe4cbb9278a96b220ea363b087376d8220a
NathalieW <nathalie.wang@isep.fr>
2017-01-20 23:57:04 +0100

    modif deploy.sh : clone de Sportymates

--- deploy.sh
+++ deploy.sh
@@ -4,12 +4,21 @@
 #tail -n +3 "$0" | ssh root@$1; exit
 #set -eu
 # deployement command
+#update/upgrade the server
+apt-get update
+apt-get upgrade
 
+#install nginx and git
+apt-get install nginx
 apt-get install git
-#cd /var/www/html
-git clone https://github.com/jonhsonofhongkong/isepweb.git
 
+cd /var/www/html
+git clone https://github.com/NathalieW/Sportymates.git
+cd /Sportymates
 
+cp accueil.html ../index.html
+
+#pour copie un fichier >> cp
 
 #update the server (~# apt-get update)
 #upgrade (~# apt-get upgrade)
